8f5d438af689ee2b849d3f2d89364cdd3cccfe9f
Amanpreet <69182131+amanpreeto7@users.noreply.github.com>
2022-07-26 12:40:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SecondKotlinClass
+ 
---
+++ app/src/main/java/com/o7services/secondkotlinclass/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.o7services.secondkotlinclass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
